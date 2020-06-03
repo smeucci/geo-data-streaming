e696fb9999819622ff54c38f6ec9638edafa13f0
Saverio <s.meucci@gmail.com>
2020-06-03 19:42:43 +0200

    add topology to count geo data by hemisphere

--- geo-data-kafka-streams/src/main/java/com/github/smeucci/geodatakafkastreams/GeoDataKafkaStreamsApplication.java
+++ geo-data-kafka-streams/src/main/java/com/github/smeucci/geodatakafkastreams/GeoDataKafkaStreamsApplication.java
@@ -2,13 +2,18 @@ package com.github.smeucci.geodatakafkastreams;
 
 import java.util.Properties;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Named;
+import org.apache.kafka.streams.kstream.Produced;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -28,6 +33,7 @@ public class GeoDataKafkaStreamsApplication implements CommandLineRunner {
 	private static final String GEO_DATA_TOPIC = "mongo.geo-data-streaming.geoData";
 	private static final String NORTHERN_HEMISPHERE_GEO_DATA_TOPIC = "northern.hemisphere.geo.data";
 	private static final String SOUTHERN_HEMISPHERE_GEO_DATA_TOPIC = "southern.hemisphere.geo.data";
+	private static final String HEMISPHERE_GEO_DATA_STATISTICS_TOPIC = "hemisphere.geo.data.statistics";
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeoDataKafkaStreamsApplication.class, args);
@@ -40,14 +46,16 @@ public class GeoDataKafkaStreamsApplication implements CommandLineRunner {
 		Properties properties = new Properties();
 		properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
 		properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
+		properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 		properties.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
 		properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
+		properties.setProperty(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0");
 
 		// create topology
 		StreamsBuilder streamsBuilder = new StreamsBuilder();
 
 		// geo data topic
-		KStream<String, String> geoDataTopic = streamsBuilder.stream(GEO_DATA_TOPIC);
+		KStream<String, String> geoDataStream = streamsBuilder.stream(GEO_DATA_TOPIC);
 
 		// predicate is in hemisphere
 		Predicate<Double> isInNorthernHemisphere = latitude -> latitude > 0;
@@ -57,47 +65,80 @@ public class GeoDataKafkaStreamsApplication implements CommandLineRunner {
 		Consumer<String> logNorthernHemisphere = geoData -> log.info("Northern Hemisphere: {}", geoData);
 		Consumer<String> logSouthernHemisphere = geoData -> log.info("Southern Hemisphere: {}", geoData);
 
+		// function get key name for hemisphere
+		Function<String, String> keyForHemisphere = geoData -> isInNorthernHemisphere
+				.test(getLatitudeFromGeoDataJson(geoData)) ? "northern_hemisphere" : "southern_hemisphere";
+
 		// filter for northern hemisphere geo data
-		KStream<String, String> northernHemisphereStream = geoDataTopic
+		KStream<String, String> northernHemisphereStream = geoDataStream
 				.filter((k, v) -> filterGeoDataByLatitude(v, isInNorthernHemisphere, logNorthernHemisphere));
 
 		// filter for southern hemisphere geo data
-		KStream<String, String> southernHemisphereStream = geoDataTopic
+		KStream<String, String> southernHemisphereStream = geoDataStream
 				.filter((k, v) -> filterGeoDataByLatitude(v, isInSouthernHemisphere, logSouthernHemisphere));
+
+		// count geo data occurrences by hemisphere
+		KStream<String, Long> hemisphereStatsStream = geoDataStream
+				// group geo data by hemisphere
+				.groupBy((k, v) -> keyForHemisphere.apply(v))
+				// count occurrences for each hemisphere
+				.count(Named.as("CountByHemisphere"))
+				// convert to stream
+				.toStream();
 
 		// set output topics
 		northernHemisphereStream.to(NORTHERN_HEMISPHERE_GEO_DATA_TOPIC);
 		southernHemisphereStream.to(SOUTHERN_HEMISPHERE_GEO_DATA_TOPIC);
+		hemisphereStatsStream.to(HEMISPHERE_GEO_DATA_STATISTICS_TOPIC, Produced.valueSerde(Serdes.Long()));
 
 		// build the topology
-		KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties);
+		Topology topology = streamsBuilder.build();
+		KafkaStreams kafkaStreams = new KafkaStreams(topology, properties);
 
 		// start the stream application
 		kafkaStreams.start();
+
+		// print topology
+		log.info("{}", topology.describe());
+
+		// add shutdown hook
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				kafkaStreams.close();
+			}
+		}));
 
 	}
 
 	private boolean filterGeoDataByLatitude(String geoDataJson, Predicate<Double> isInThisHemisphere,
 			Consumer<String> logThisHemisphere) {
 
+		Double latitude = getLatitudeFromGeoDataJson(geoDataJson);
+
+		boolean inThisHemisphere = isInThisHemisphere.test(latitude);
+
+		if (inThisHemisphere && logThisHemisphere != null) {
+			logThisHemisphere.accept(geoDataJson);
+		}
+
+		return inThisHemisphere;
+
+	}
+
+	private double getLatitudeFromGeoDataJson(String geoDataJson) {
+
 		try {
 
-			Double latitude = JsonParser.parseString(geoDataJson) //
+			return JsonParser.parseString(geoDataJson) //
 					.getAsJsonObject() //
 					.get("latitude") //
 					.getAsDouble();
 
-			boolean inThisHemisphere = isInThisHemisphere.test(latitude);
-
-			if (inThisHemisphere) {
-				logThisHemisphere.accept(geoDataJson);
-			}
-
-			return inThisHemisphere;
-
 		} catch (Exception e) {
 
-			return false;
+			return 0;
 
 		}
 
