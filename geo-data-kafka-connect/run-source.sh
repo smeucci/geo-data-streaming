76c95f54ff02f98c89931a45c19a0220743a6337
Saverio <s.meucci@gmail.com>
2020-06-03 14:47:56 +0200

    create tmp hidden folder for kafka connect

--- geo-data-kafka-connect/run-source.sh
+++ geo-data-kafka-connect/run-source.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# create tmp folder for connect offsets
+mkdir -p .tmp-connect
+
 # run the mongo source connector
 connect-standalone.sh connect-standalone.properties mongo-source-connector.properties
