9908118a5c49629a83f6ec8c8c7519c6f3f82c1b
Saverio <s.meucci91@gmail.com>
2020-06-05 22:48:15 +0200

    set bootstrap server with env variable

--- geo-data-kafka-streams/src/main/java/com/github/smeucci/geo/data/kafka/streams/GeoDataKafkaStreamsApplication.java
+++ geo-data-kafka-streams/src/main/java/com/github/smeucci/geo/data/kafka/streams/GeoDataKafkaStreamsApplication.java
@@ -4,6 +4,7 @@ import java.util.Properties;
 
 import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +15,10 @@ import com.github.smeucci.geo.data.kafka.streams.utils.GeoDataUtils;
 @SpringBootApplication
 public class GeoDataKafkaStreamsApplication implements CommandLineRunner {
 
-	private static final String BOOTSTRAP_SERVER = "localhost:9092";
 	private static final String APPLICATION_ID = "geo-data-kafka-streams";
+
+	@Value("${KAFKA_URI:localhost:9092}")
+	private String bootstrapServer;
 
 	@Autowired
 	private GeoDataStream geoDataStream;
@@ -29,7 +32,7 @@ public class GeoDataKafkaStreamsApplication implements CommandLineRunner {
 
 		// create properties
 		Properties properties = GeoDataUtils.properties();
-		properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+		properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
 		properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
 
 		geoDataStream
