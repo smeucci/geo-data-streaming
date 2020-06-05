dc74b5751feaf9f7cf4e304faeb35eddcb34998d
Saverio <s.meucci91@gmail.com>
2020-06-05 22:01:14 +0200

    set zk-kafka container in docker-compose.yml

--- reset-kafka.sh
+++ reset-kafka.sh
@@ -8,19 +8,19 @@ rm -rf geo-data-kafka-connect/.tmp-connect
 
 # create mongo geo data topic
 echo "creating mongo geo data topic..."
-docker exec -it kafka kafka-topics --zookeeper zookeeper:2181 --create --topic mongo.geo-data-streaming.geoData --partitions 3 --replication-factor 1
+docker exec -it kafka-geo-data-streaming kafka-topics --zookeeper zookeeper-geo-data-streaming:2181 --create --topic mongo.geo-data-streaming.geoData --partitions 3 --replication-factor 1
 
 # create northern hemisphere geo data topic
 echo "creating northern hemisphere geo data topic..."
-docker exec -it kafka kafka-topics --zookeeper zookeeper:2181 --create --topic northern.hemisphere.geo.data --partitions 3 --replication-factor 1
+docker exec -it kafka-geo-data-streaming kafka-topics --zookeeper zookeeper-geo-data-streaming:2181 --create --topic northern.hemisphere.geo.data --partitions 3 --replication-factor 1
 
 # create southern hemisphere geo data topic
 echo "creating southern hemisphere geo data topic..."
-docker exec -it kafka kafka-topics --zookeeper zookeeper:2181 --create --topic southern.hemisphere.geo.data --partitions 3 --replication-factor 1
+docker exec -it kafka-geo-data-streaming kafka-topics --zookeeper zookeeper-geo-data-streaming:2181 --create --topic southern.hemisphere.geo.data --partitions 3 --replication-factor 1
 
 # create hemisphere geo data statistics topic
 echo "creating hemisphere geo data statistics topic..."
-docker exec -it kafka kafka-topics --zookeeper zookeeper:2181 --create --topic hemisphere.geo.data.statistics --partitions 3 --replication-factor 1 \
+docker exec -it kafka-geo-data-streaming kafka-topics --zookeeper zookeeper-geo-data-streaming:2181 --create --topic hemisphere.geo.data.statistics --partitions 3 --replication-factor 1 \
 --config cleanup.policy=compact \
 --config min.cleanable.dirty.ratio=0.005 \
 --config segment.ms=10000
