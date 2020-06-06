4e31c57efa0bcbebd7a5aed5b4e3bf4b6082279a
Saverio <s.meucci91@gmail.com>
2020-06-06 17:01:39 +0200

    write start.sh inside dockerfile

--- geo-data-kafka-streams/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# sleep for 10 seconds waiting for kafka setup
-echo "Waiting for 10 seconds..."
-sleep 10
-
-# start the app
-echo "Starting geo-data-kafka-streams..."
-java -jar ./geo-data-kafka-streams.jar
